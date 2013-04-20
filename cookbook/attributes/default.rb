a8d57d79e23e7d8811cfb718f83b6f228957fe21
Will Maier <wcmaier@m.aier.us>
2013-04-20 16:37:38 +0000

    Add repo.

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:dusk][:user] = 'dusk'
 default[:dusk][:root] = '/opt/dusk'
 default[:dusk][:revision] = 'master'
+default[:dusk][:repo] = 'git@github.com:obfuscurity/dusk'
 default[:dusk][:env] = {
 }

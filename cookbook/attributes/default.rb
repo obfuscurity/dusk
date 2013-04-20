59dbd9fcb5890c3222e05d048c28048ebcfa5377
Will Maier <wcmaier@m.aier.us>
2013-04-20 17:21:37 +0000

    HTTPS github.

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:dusk][:user] = 'dusk'
 default[:dusk][:root] = '/opt/dusk'
 default[:dusk][:revision] = 'master'
-default[:dusk][:repo] = 'git@github.com:obfuscurity/dusk'
+default[:dusk][:repo] = 'https://github.com/obfuscurity/dusk.git'
 default[:dusk][:env] = {
   'GRAPHITE_URL' => 'http://graphite/'
 }

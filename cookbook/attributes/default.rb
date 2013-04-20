b6328de00069c938b6d1ae5f09f045032d425318
Will Maier <wcmaier@m.aier.us>
2013-04-20 18:12:48 +0000

    Set PORT.

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -3,5 +3,6 @@ default[:dusk][:root] = '/opt/dusk'
 default[:dusk][:revision] = 'master'
 default[:dusk][:repo] = 'https://github.com/obfuscurity/dusk.git'
 default[:dusk][:env] = {
-  'GRAPHITE_URL' => 'http://graphite/'
+  'GRAPHITE_URL' => 'http://graphite/',
+  'PORT' => '5000'
 }

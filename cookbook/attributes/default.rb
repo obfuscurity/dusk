716775b8e2ad4c7fe920afde288972577739a3d9
Will Maier <wcmaier@m.aier.us>
2013-04-20 16:43:55 +0000

    Set a reasonable default for GRAPHITE_URL.

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -3,4 +3,5 @@ default[:dusk][:root] = '/opt/dusk'
 default[:dusk][:revision] = 'master'
 default[:dusk][:repo] = 'git@github.com:obfuscurity/dusk'
 default[:dusk][:env] = {
+  'GRAPHITE_URL' => 'http://graphite/'
 }
